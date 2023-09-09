327477c22fafa6d26218db851e730436039dbedf
Alzhanbaev Kamchybek <aljanbaevkam@gmail.com>
2023-09-09 15:12:52 +0600

    First commit

---
+++ src/main/java/com/example/eshopfirstproject/EShopFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.eshopfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EShopFirstProjectApplication.class, args);
+    }
+
+}
